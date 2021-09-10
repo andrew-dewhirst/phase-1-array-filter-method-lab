03860e3135c2d199c6e9544e106524234c812fc6
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-09 20:55:20 -0400

    commit tree 3fe0a89c84eaf0ffe9df88a71072bb96da187993

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivingArray, name) {
+    return drivingArray.filter(function(obj) {
+        return obj.toLowerCase() === name.toLowerCase();
+    });
+  }
+
+function fuzzyMatch(drivingArray, letters) {
+    return drivingArray.filter(obj => obj.startsWith(letters));
+}
+
+function matchName(drivingArray,driver){
+    return drivingArray.filter(obj => obj.name === driver);
+}
